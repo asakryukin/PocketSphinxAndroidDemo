28aa359c3c6a03cc8abc08392e320b7bdad9f0ed
asakryukin <asakryukin@gmail.com>
2015-02-05 19:31:46 +0600

    Revert 957d4aa..787c337
    This rolls back to commit 957d4aab929b021cd5168f75261ad056cd8bb1c1.

--- jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := PocketSphinxAndroidDemo
-LOCAL_SRC_FILES := PocketSphinxAndroidDemo.cpp
-
-include $(BUILD_SHARED_LIBRARY)
