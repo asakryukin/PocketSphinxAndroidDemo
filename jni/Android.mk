f30b591ce201163ce1c59400633dbd748ed3ecd4
asakryukin <asakryukin@gmail.com>
2015-02-10 13:42:50 +0600

    fail

---
+++ jni/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+OPENCV_LIB_TYPE:=STATIC
+include ../../../Downloads/OpenCV-2.4.9-android-sdk/sdk/native/jni/OpenCV.mk
+
+LOCAL_MODULE    := PocketSphinxAndroidDemo
+LOCAL_SRC_FILES := PocketSphinxAndroidDemo.cpp
+
+include $(BUILD_SHARED_LIBRARY)
