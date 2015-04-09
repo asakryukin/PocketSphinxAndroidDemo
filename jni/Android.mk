cdb052f2764bd24f90b34614dae0bcfb3d5bf4d0
asakryukin <asakryukin@gmail.com>
2015-04-09 16:56:53 +0600

    Revert 32d4b26..e2a1850
    This rolls back to commit 32d4b2660f131ba93c2802098d86ce5f60e1e79d.

--- jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-# OpenCV
-OPENCV_CAMERA_MODULES:=on
-OPENCV_INSTALL_MODULES:=on
-OPENCV_LIB_TYPE:=STATIC
-include ../../sdk/native/jni/OpenCV.mk
